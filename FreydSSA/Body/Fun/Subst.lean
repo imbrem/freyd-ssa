07eca97011162a75d322b8628916b47c3de4e821
Jad Ghalayini <jad.ghalayini@protonmail.com>
2024-04-11 11:06:47 +0300

    Began UBody substitution

--- FreydSSA/Body/Fun/Subst.lean
+++ FreydSSA/Body/Fun/Subst.lean
@@ -1,1 +1,13 @@
 import FreydSSA.Body.Fun.Basic
+import FreydSSA.Term.Fun.Subst
+
+variable {φ ν α} [Φ : InstSet φ (Ty α)]
+  [Φc : CohInstSet φ (Ty α)]
+  [DecidableEq ν] [DecidableEq α]
+
+--TODO: _capture avoiding_ substitution of σ, so as not to change newly bound variables
+--TODO: also, try _minimal new substitution_, and so on...
+-- def UBody.FWf.Subst {Γ' Γ Δ : FCtx ν (Ty α)} {t : UBody φ ν} {σ : ν → UTm φ ν}
+--   (hσ : Γ'.Subst σ Γ) (dt : t.FWf p Γ Δ)
+--   : (Δ' : FCtx ν (Ty α)) × t.FWf p Γ' Δ' × Δ'.Subst σ Δ
+--   := sorry
