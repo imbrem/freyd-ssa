2e44c0f2f3cf97595e96148f1f359fd54f12617e
Jad Ghalayini <jad.ghalayini@protonmail.com>
2024-04-11 19:44:26 +0300

    Naming

--- FreydSSA/Ctx/Label/Fun.lean
+++ FreydSSA/Ctx/Label/Fun.lean
@@ -5,63 +5,63 @@ variable {ν ν' κ κ' α β}
   [DecidableEq κ] [DecidableEq κ']
   [DecidableEq α] [DecidableEq β]
 
-structure Label (ν : Type _) (α : Type _) where
+structure FLabel (ν : Type _) (α : Type _) where
   -- TODO: Fun with parameter contexts? Parameter sets? Many options...
   param : α
   live : FCtx ν α
 
-theorem Label.ext {L K : Label ν α} (h : L.param = K.param) (h' : L.live = K.live)
+theorem FLabel.ext {L K : FLabel ν α} (h : L.param = K.param) (h' : L.live = K.live)
   : L = K := by
   cases L; cases K
   cases h; cases h'
   rfl
 
-structure Label.Wk (L K : Label ν α) : Prop where
+structure FLabel.Wk (L K : FLabel ν α) : Prop where
   param : L.param = K.param
   live : L.live.Wk K.live
 
-theorem Label.Wk.refl (L : Label ν α) : Label.Wk L L := ⟨rfl, FCtx.Wk.refl _⟩
-theorem Label.Wk.comp {L K M : Label ν α} (h : Label.Wk L K) (h' : Label.Wk K M)
-  : Label.Wk L M := ⟨h.param ▸ h'.param, h.live.comp h'.live⟩
-theorem Label.Wk.antisymm {L K : Label ν α} (h : Label.Wk L K) (h' : Label.Wk K L)
-  : L = K := Label.ext h.param (h.live.antisymm h'.live)
+theorem FLabel.Wk.refl (L : FLabel ν α) : FLabel.Wk L L := ⟨rfl, FCtx.Wk.refl _⟩
+theorem FLabel.Wk.comp {L K M : FLabel ν α} (h : FLabel.Wk L K) (h' : FLabel.Wk K M)
+  : FLabel.Wk L M := ⟨h.param ▸ h'.param, h.live.comp h'.live⟩
+theorem FLabel.Wk.antisymm {L K : FLabel ν α} (h : FLabel.Wk L K) (h' : FLabel.Wk K L)
+  : L = K := FLabel.ext h.param (h.live.antisymm h'.live)
 
-instance Label.instPartialOrder : PartialOrder (Label ν α) where
-  le L K := Label.Wk K L
-  le_refl _ := Label.Wk.refl _
+instance FLabel.instPartialOrder : PartialOrder (FLabel ν α) where
+  le L K := FLabel.Wk K L
+  le_refl _ := FLabel.Wk.refl _
   le_trans _ _ _ h h' := h'.comp h
   le_antisymm _ _ h h' := Wk.antisymm h' h
 
-structure Label.Cmp (L K : Label ν α) : Prop where
+structure FLabel.Cmp (L K : FLabel ν α) : Prop where
   param : L.param = K.param
   live : L.live.Cmp K.live
 
-theorem Label.Cmp.refl (L : Label ν α) : Label.Cmp L L := ⟨rfl, FCtx.Cmp.refl _⟩
-theorem Label.Cmp.symm {L K : Label ν α} (h : Label.Cmp L K) : Label.Cmp K L
+theorem FLabel.Cmp.refl (L : FLabel ν α) : FLabel.Cmp L L := ⟨rfl, FCtx.Cmp.refl _⟩
+theorem FLabel.Cmp.symm {L K : FLabel ν α} (h : FLabel.Cmp L K) : FLabel.Cmp K L
   := ⟨h.param.symm, h.live.symm⟩
 
-def Label.linf (L K : Label ν α) : Label ν α where
+def FLabel.linf (L K : FLabel ν α) : FLabel ν α where
   param := L.param
   live := L.live.linf K.live
 
-def Label.rinf (L K : Label ν α) : Label ν α where
+def FLabel.rinf (L K : FLabel ν α) : FLabel ν α where
   param := K.param
   live := L.live.rinf K.live
 
-def Label.inf (L K : Label ν α) : Label ν α where
+def FLabel.inf (L K : FLabel ν α) : FLabel ν α where
   param := L.param
   live := L.live.inf K.live
 
-def Label.lsup (L K : Label ν α) : Label ν α where
+def FLabel.lsup (L K : FLabel ν α) : FLabel ν α where
   param := L.param
   live := L.live.lsup K.live
 
-def Label.rsup (L K : Label ν α) : Label ν α where
+def FLabel.rsup (L K : FLabel ν α) : FLabel ν α where
   param := K.param
   live := L.live.rsup K.live
 
 structure FLCtx (κ : Type _) (ν : Type _) (α : Type _) : Type _ where
-  toFun : κ → WithTop (Label ν α)
+  toFun : κ → WithTop (FLabel ν α)
   support : Finset κ
   mem_support_toFun : ∀x, x ∈ support ↔ toFun x ≠ ⊤
 
@@ -81,7 +81,7 @@ theorem FLCtx.toFun_inj {L K : FLCtx κ ν α}
 
 --TODO: Injective instance? Naming convention...
 
-instance FLCtx.instFunLike : FunLike (FLCtx κ ν α) κ (WithTop (Label ν α)) where
+instance FLCtx.instFunLike : FunLike (FLCtx κ ν α) κ (WithTop (FLabel ν α)) where
   coe := FLCtx.toFun
   coe_injective' := by intro Γ Δ; apply FLCtx.toFun_inj_mp
 
@@ -137,20 +137,20 @@ theorem FLCtx.PWk.to_wk {L K : FLCtx κ ν α} (h : FLCtx.PWk L K) : FLCtx.Wk L 
 theorem FLCtx.PWk.antisymm {L K : FLCtx κ ν α} (h : FLCtx.PWk L K) (h' : FLCtx.PWk K L)
   : L = K := h.to_wk.antisymm h'.to_wk
 
-def FLCtx.CmpTop : WithTop (Label ν α) → WithTop (Label ν α) → Prop
+def FLCtx.CmpTop : WithTop (FLabel ν α) → WithTop (FLabel ν α) → Prop
   | ⊤, _ => true
   | _, ⊤ => true
   | some Γ, some Δ => Γ.Cmp Δ
 
-theorem FLCtx.CmpTop.refl (Γ : WithTop (Label ν α)) : FLCtx.CmpTop Γ Γ
+theorem FLCtx.CmpTop.refl (Γ : WithTop (FLabel ν α)) : FLCtx.CmpTop Γ Γ
   := match Γ with
   | ⊤ => by trivial
-  | some Γ => Label.Cmp.refl Γ
+  | some Γ => FLabel.Cmp.refl Γ
 
-theorem FLCtx.CmpTop.symm {Γ Δ : WithTop (Label ν α)} (h : FLCtx.CmpTop Γ Δ)
+theorem FLCtx.CmpTop.symm {Γ Δ : WithTop (FLabel ν α)} (h : FLCtx.CmpTop Γ Δ)
   : FLCtx.CmpTop Δ Γ := by
   cases Γ <;> cases Δ
-  case some.some => exact Label.Cmp.symm h
+  case some.some => exact FLabel.Cmp.symm h
   all_goals exact h
 
 def FLCtx.Cmp (L K : FLCtx κ ν α) : Prop := ∀x, CmpTop (L x) (K x)
@@ -159,30 +159,30 @@ theorem FLCtx.Cmp.refl (L : FLCtx κ ν α) : FLCtx.Cmp L L := λ_ => FLCtx.CmpT
 theorem FLCtx.Cmp.symm {L K : FLCtx κ ν α} (h : FLCtx.Cmp L K) : FLCtx.Cmp K L
   := λx => FLCtx.CmpTop.symm (h x)
 
-def FLCtx.linf_top : WithTop (Label ν α) → WithTop (Label ν α) → WithTop (Label ν α)
+def FLCtx.linf_top : WithTop (FLabel ν α) → WithTop (FLabel ν α) → WithTop (FLabel ν α)
   | ⊤, x => x
   | x, ⊤ => x
-  | some Γ, some Δ => some (Label.linf Γ Δ)
+  | some Γ, some Δ => some (FLabel.linf Γ Δ)
 
-def FLCtx.rinf_top : WithTop (Label ν α) → WithTop (Label ν α) → WithTop (Label ν α)
+def FLCtx.rinf_top : WithTop (FLabel ν α) → WithTop (FLabel ν α) → WithTop (FLabel ν α)
   | ⊤, x => x
   | x, ⊤ => x
-  | some Γ, some Δ => some (Label.rinf Γ Δ)
+  | some Γ, some Δ => some (FLabel.rinf Γ Δ)
 
-def FLCtx.inf_top : WithTop (Label ν α) → WithTop (Label ν α) → WithTop (Label ν α)
+def FLCtx.inf_top : WithTop (FLabel ν α) → WithTop (FLabel ν α) → WithTop (FLabel ν α)
   | ⊤, _ => ⊤
   | _, ⊤ => ⊤
-  | some Γ, some Δ => if Γ.param = Δ.param then some (Label.inf Γ Δ) else ⊤
+  | some Γ, some Δ => if Γ.param = Δ.param then some (FLabel.inf Γ Δ) else ⊤
 
-def FLCtx.lsup_top : WithTop (Label ν α) → WithTop (Label ν α) → WithTop (Label ν α)
+def FLCtx.lsup_top : WithTop (FLabel ν α) → WithTop (FLabel ν α) → WithTop (FLabel ν α)
   | ⊤, _ => ⊤
   | _, ⊤ => ⊤
-  | some Γ, some Δ => some (Label.lsup Γ Δ)
+  | some Γ, some Δ => some (FLabel.lsup Γ Δ)
 
-def FLCtx.rsup_top : WithTop (Label ν α) → WithTop (Label ν α) → WithTop (Label ν α)
+def FLCtx.rsup_top : WithTop (FLabel ν α) → WithTop (FLabel ν α) → WithTop (FLabel ν α)
   | ⊤, _ => ⊤
   | _, ⊤ => ⊤
-  | some Γ, some Δ => some (Label.rsup Γ Δ)
+  | some Γ, some Δ => some (FLabel.rsup Γ Δ)
 
 -- def FLCtx.linf (L K : FLCtx κ ν α) : FLCtx κ ν α where
 --   toFun x := linf_top (L x) (K x)
