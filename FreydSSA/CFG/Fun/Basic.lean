877043d7e8bc138e7e3b4bf0f37bb76624cdd65a
Jad Ghalayini <jad.ghalayini@protonmail.com>
2024-04-17 17:49:27 +0300

    Revert "cons ==> update"
    This reverts commit a2e10a1967c92ba7fc4736bba419ba5e38a42c73.

--- FreydSSA/CFG/Fun/Basic.lean
+++
@@ -1,9 +0,0 @@
-import FreydSSA.BasicBlock.Fun.Basic
-
-variable {φ : Type u₁} {ν : Type u₂} {κ : Type u₃} {α : Type u₄} [Φ : InstSet φ (Ty α)]
-  [Φc : CohInstSet φ (Ty α)]
-  [DecidableEq ν] [DecidableEq κ] [DecidableEq α]
-
--- inductive UCFG.FWfI : FLCtx κ ν (Ty α) → UCFG φ (Ty α) ν κ → FLCtx κ ν (Ty α) → Type _
---   | nil : L.Wk K → FWfI L nil K
---   | cons (ℓ x A) : FWfI L g sorry → β.FWf 0 sorry L → FWfI L (g.cons ℓ x A β) K
